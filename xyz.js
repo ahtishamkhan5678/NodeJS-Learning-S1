f9b92d68095cd1fa6d261648b3159772e1faa43b
mohammadAhtisham <mohammadahtisham@mohammadAhtishams-MacBook-Air.local>
2025-08-21 16:41:38 +0530

    seaon1-notes

--- xyz.js
+++ xyz.js
@@ -1,1 +1,13 @@
 console.log("it is xyz module");
+
+// NOTE : Lect : 5 
+// require("./path") it wrap the code then it runs 
+// All the code of the module is wrapped inside a function and that function is know as IIFE 
+
+// IIFE - Immediately Invoked function Expression belwow the example
+
+(function (){
+    
+   // All code of the module run inside here 
+
+})();
